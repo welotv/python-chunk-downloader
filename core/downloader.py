59a25a8c15716e16801a701a1cf61d2809513014
Diego <diegolis@gmail.com>
2016-11-25 15:35:04 -0300

    properties

--- core/downloader.py
+++ core/downloader.py
@@ -8,13 +8,18 @@ class HTTPDownloader:
         self.last_byte = 0
         self.size = size
 
+    @property
+    def new_last_byte(self):
+        return self.last_byte + self.size
+
+    @property
+    def range_(self):
+        return (self.last_byte, self.new_last_byte - 1)
+
     def get_chunk(self):
         req = urllib2.Request(self.url)
-
-        new_last_byte = self.last_byte + self.size
-        req.headers['Range'] = 'bytes=%s-%s' % (
-            self.last_byte, new_last_byte - 1)
-        self.last_byte = new_last_byte
-
+        req.headers['Range'] = 'bytes=%s-%s' % self.range_
         f = urllib2.urlopen(req)
+        self.last_byte = self.new_last_byte
+
         return f.read()
